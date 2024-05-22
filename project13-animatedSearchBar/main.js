a56b35b15a38df9fec87e5e6430f8f37de585b7c
hamdytoto <hamdytoto>
2024-05-22 19:22:19 +0300

    adding the project 13

---
+++ project13-animatedSearchBar/main.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".search-bar-container");
+const magni =document.querySelector(".magni");
+magni.addEventListener("click",()=>{
+    container.classList.toggle("active");
+})
