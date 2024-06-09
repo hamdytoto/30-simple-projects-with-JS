333fe92dd43cc378b5cf46abc3a4618ac4323faf
hamdytoto <hamdytoto>
2024-06-09 19:51:00 +0300

    the project of clock

---
+++ project21-colock/main.js
@@ -0,0 +1,32 @@
+let Hours = document.querySelector(".hours");
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let creator = document.querySelector(".creator");
+function getRandomColor() {
+	const letters = "0123456789ABCDEF";
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+// Example usage:
+console.log(getRandomColor()); // e.g. #3E2F1B
+
+function updateTime() {
+	let currentDate = new Date();
+	let hoursDeg = (Hours.style.transform = `rotate(${
+		(currentDate.getHours() / 12) * 360
+	}deg)`);
+	let minutesdeg = (minutes.style.transform = `rotate(${
+		(currentDate.getMinutes() / 60) * 360
+	}deg)`);
+	let secondsdeg = (seconds.style.transform = `rotate(${
+		(currentDate.getSeconds() / 60) * 360
+	}deg)`);
+}
+setInterval(() => {
+	updateTime();
+	creator.style.color = getRandomColor();
+    seconds.style.backgroundColor=getRandomColor();
+}, 1000);
