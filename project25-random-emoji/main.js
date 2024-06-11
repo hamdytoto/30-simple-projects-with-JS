6c799eae0b1ddcfb24b0b892b3512522bcd0e390
hamdytoto <hamdytoto>
2024-06-11 22:59:36 +0300

    adding some details

--- project25-random-emoji/main.js
+++ project25-random-emoji/main.js
@@ -1,17 +1,3 @@
-async function getEmjis() {
-	let response = await fetch(
-		"https://emoji-api.com/emojis?access_key=aa5524fd1b5f28e7f42f60b5a207dc3a1186d3c0"
-	);
-	data = await response.json();
-	for (let i = 0; i < 1000; i++) {
-		Emojis.push({
-			emojyShape: data[i].character,
-			emojyName: data[i].unicodeName,
-		});
-	}
-	console.log(Emojis);
-}
-getEmjis();
 createElements();
 function createElements() {
 	let title = document.createElement("h2");
@@ -32,8 +18,8 @@ function createElements() {
 	EmojiCont.style.cssText =
 		"height:300px;width:300px;display:flex;justify-content:space-between;align-items:center;flex-direction:column;padding:20px";
 	EmojiBtn.style.cssText =
-		"padding:20px;border:none;background-color:none; width:300px; height:300px";
-	EmojiName.style.cssText="font-size:30px";
+		"padding:20px;border:none;background-color:none; border-raduis width:300px; height:300px; font-size:100px;";
+	EmojiName.style.cssText = "font-size:30px";
 	EmojiBtn.addEventListener("click", () => {
 		let randomNum = Math.floor(Math.random() * Emojis.length);
 		EmojiBtn.innerText = Emojis[randomNum].emojyShape;
@@ -41,3 +27,17 @@ function createElements() {
 	});
 }
 const Emojis = [];
+async function getEmjis() {
+	let response = await fetch(
+		"https://emoji-api.com/emojis?access_key=aa5524fd1b5f28e7f42f60b5a207dc3a1186d3c0"
+	);
+	data = await response.json();
+	for (let i = 0; i < 1000; i++) {
+		Emojis.push({
+			emojyShape: data[i].character,
+			emojyName: data[i].unicodeName,
+		});
+	}
+	console.log(Emojis);
+}
+getEmjis();
